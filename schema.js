9909052dbd3741e7ad242065cd0d43cddb4d946c
Nuno <ngocalves@gmail.com>
2019-08-18 17:16:09 +0200

    GraphQL server with popular, newest and featured things

--- schema.js
+++ schema.js
@@ -1,16 +1,75 @@
+const thingiverse = require('thingiverse-js')
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema
+  GraphQLBoolean,
+  GraphQLSchema,
+  GraphQLList
 } = require('graphql')
+
+const token = process.env.THINGVERSE_KEY
+
+const loadThings = type =>
+  thingiverse(type, { token })
+    .then(res => res.body)
+    .catch(err => {
+      console.log(err)
+      console.log(thingiverse.getError(err.response))
+    })
 
 const ThingType = new GraphQLObjectType({
   name: 'Thing',
   fields: () => ({
     id: { type: GraphQLInt },
-    name: { type: GraphQLString }
+    name: { type: GraphQLString },
+    url: { type: GraphQLString },
+    public_url: { type: GraphQLString },
+    thumbnail: { type: GraphQLString },
+    creator: { type: CreatorType },
+    is_private: { type: GraphQLBoolean },
+    is_purchased: { type: GraphQLBoolean },
+    is_published: { type: GraphQLBoolean }
   })
 })
 
-module.exports = new GraphQLSchema()
+const CreatorType = new GraphQLObjectType({
+  name: 'Creator',
+  fields: () => ({
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    first_name: { type: GraphQLString },
+    last_name: { type: GraphQLString },
+    url: { type: GraphQLString },
+    public_url: { type: GraphQLString },
+    thumbnail: { type: GraphQLString }
+  })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQuery',
+  fields: {
+    popular: {
+      type: GraphQLList(ThingType),
+      resolve(parent, args) {
+        return loadThings('popular')
+      }
+    },
+    newest: {
+      type: GraphQLList(ThingType),
+      resolve(parent, args) {
+        return loadThings('newest')
+      }
+    },
+    featured: {
+      type: GraphQLList(ThingType),
+      resolve(parent, args) {
+        return loadThings('featured')
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema')
 
-app.get('/data', (req, res, next) => {
-  res.json(['Tony', 'Lisa', 'Michael', 'Ginger', 'Food'])
-})
+const app = express()
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000')
-})
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
